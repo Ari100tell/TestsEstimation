373c72fdc52df057574da8b23bde09ae12f1c6f2
Ari100tell <alex111544@ukr.net>
2014-03-17 03:47:20 +0200

    Created class for testing hibernate connection
    Created class that realize hibernate connection for database;
    Created example for testing connection and getting or saving information
    in database

---
+++ src/test/DAO/Factory.java
@@ -0,0 +1,23 @@
+package test.DAO;
+
+import test.DAO.Impl.StudentDAOImpl;
+
+public class Factory {
+
+    private static StudentDAO studentDAO = null;
+    private static Factory instance = null;
+
+    public static synchronized Factory getInstance(){
+        if (instance == null){
+            instance = new Factory();
+        }
+        return instance;
+    }
+
+    public StudentDAO getStudentDAO(){
+        if (studentDAO == null){
+            studentDAO = new StudentDAOImpl();
+        }
+        return studentDAO;
+    }
+}
---
+++ src/test/DAO/Impl/StudentDAOImpl.java
@@ -0,0 +1,95 @@
+package test.DAO.Impl;
+
+
+import org.hibernate.Session;
+import test.DAO.StudentDAO;
+import test.logic.Student;
+import test.util.HibernateUtil;
+
+import javax.swing.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDAOImpl implements StudentDAO {
+
+    public void addStudent(Student stud) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(stud);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public void updateStudent(Student stud) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(stud);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public Student getStudentById(Long id) throws SQLException {
+        Session session = null;
+        Student stud = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            stud = (Student) session.load(Student.class, id);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return stud;
+    }
+
+    public List<Student> getAllStudents() throws SQLException {
+        Session session = null;
+        List<Student> studs = new ArrayList<Student>();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            studs = session.createCriteria(Student.class).list();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return studs;
+    }
+
+    public void deleteStudent(Student stud) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(stud);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+}
